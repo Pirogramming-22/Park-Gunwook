45ece65094f24b161ff4a2486603015b31db7da3
parkgunwook0617 <ss944161269441@gmail.com>
2024-12-27 02:50:02 +0900

    Delete a.py

--- a.py
+++
@@ -1,8 +0,0 @@
-a, b, c, d = map(int, input().split())
-if a > 100 or a < 0 or b > 100 or b < 0 or c > 100 or c < 0 or d > 100 or d < 0:
-    print('잘못된 점수')
-    exit()
-if ((a + b + c + d) / 4) >= 80:
-    print("합격")
-else:
-    print("불합격")
