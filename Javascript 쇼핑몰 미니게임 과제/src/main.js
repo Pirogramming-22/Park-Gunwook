5f7217c7dae5a5ff5255a881956ef7669a1afa3d
parkgunwook <ss944161269441@gmail.com>
2024-12-31 23:01:33 +0900

    JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,13 +1,28 @@
 function loadItems() {
     return fetch('data/data.json')
-    .then(response => response.json())
-    .then(json => json.items);
+      .then(response => response.json())
+      .then(json => json.items);
+}
+  
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    const html = items.map(item => createHTMLString(item));
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+  
+function createHTMLString(item) {
+    return `
+      <li class="item">
+          <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+          <span class="item__description">${item.gender}, ${item.size}</span>
+      </li>
+    `;
 }
 
 loadItems()
-.then(items => {
-    console.log(items);
-    //displayItems(items);
-    //setEventListeners(items);
-})
-.catch(console.log)
+    .then(items => {
+      displayItems(items);
+      // setEventListeners(items);
+    })
+    .catch(console.log);
+  
