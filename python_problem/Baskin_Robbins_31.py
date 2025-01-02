a1d5994d59e882dbe2b0130b6a74804f6e0c3220
parkgunwook <ss944161269441@gmail.com>
2025-01-02 16:50:35 +0900

    8단계

--- python_problem/Baskin_Robbins_31.py
+++ python_problem/Baskin_Robbins_31.py
@@ -1,22 +1,23 @@
 num = 0
 playerATurn = True
 
-while True:
-    try:
-        playerNum = float(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : "))
-        if playerNum in [1, 2, 3]:
-            playerNum = int(playerNum)
-            pass    
-        elif playerNum.is_integer():
-            print("1, 2, 3 중 하나를 입력하세요.")
-            continue
-        else:
+def brGame():
+    while True:
+        try:
+            playerNum = float(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : "))
+            if playerNum in [1, 2, 3]:
+                return int(playerNum)
+            elif playerNum.is_integer():
+                print("1, 2, 3 중 하나를 입력하세요.")
+            else:
+                print("정수를 입력하세요")
+        except ValueError:
             print("정수를 입력하세요")
-            continue
-    except ValueError:
-        print("정수를 입력하세요")
-        continue
+        
 
+while True:
+    playerNum = brGame()
+    
     for _ in range(playerNum):
         num += 1
         print(f"{'playerA' if playerATurn else 'playerB'} : {num}")
