44a96e81c07af06b58ca009041af86324a7b760f
parkgunwook <ss944161269441@gmail.com>
2025-01-02 16:43:41 +0900

    5단계

--- python_problem/Baskin_Robbins_31.py
+++ python_problem/Baskin_Robbins_31.py
@@ -1,13 +1,11 @@
 num = 0
+playerATurn = True
 
 while True:
     try:
         playerNum = float(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : "))
         if playerNum in [1, 2, 3]:
             playerNum = int(playerNum)
-            for i in range(playerNum):
-                num += 1
-                print(f"playerA : {num}")
             pass    
         elif playerNum.is_integer():
             print("1, 2, 3 중 하나를 입력하세요.")
@@ -18,3 +16,12 @@ while True:
     except ValueError:
         print("정수를 입력하세요")
         continue
+
+    for _ in range(playerNum):
+        num += 1
+        print(f"{'playerA' if playerATurn else 'playerB'} : {num}")
+        if num >= 31:
+            break
+    
+    playerATurn = not playerATurn
+    
