c44b451fd52feb014a56cfd6385cb5d788bc3a0d
parkgunwook <ss944161269441@gmail.com>
2025-01-07 17:30:02 +0900

    나의 첫 번째 Django 프로젝트!

--- djangogirls/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\user\Desktop\박건욱\프로그래밍\PIROGRAMMING 활동기록\Piro22실습\Park-Gunwook\djangogirls\myvenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
